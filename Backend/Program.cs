8359cc415e9f46672f5609ae215c3f41f4b4f9f6
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-04 11:32:31 -0300

    first commit

---
+++ Backend/Program.cs
@@ -0,0 +1,43 @@
+// imports
+using dotenv.net;
+
+DotEnv.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(
+        builder => {
+            builder.WithOrigins("http://localhost:3000");
+            builder.AllowAnyHeader();
+            builder.AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
