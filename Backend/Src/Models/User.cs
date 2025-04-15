7a881445cbf53ba01b9b965f2035a61ef8af3ca3
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 11:58:57 -0400

    feat(User): added User parameters that Admins can view

--- Backend/Src/Models/User.cs
+++ Backend/Src/Models/User.cs
@@ -11,6 +11,11 @@ public class User
     public required string PhoneNumber { get; set; };
     public required DateOnly BirthDate { get; set; };
     public required string Password { get; set; };
+    // Only Admins can view this field
+    public string Status { get; set; } = "Active";
+    public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public DateOnly LastLogin { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
     // Class Adress not needed on register form
     public Adress? Adress { get; set; };
 }
