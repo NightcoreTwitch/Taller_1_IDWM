b35f584a495a9fe712be5b204e19d90163a08daa
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 11:01:00 -0400

    feat(User): added User parameters

--- Backend/Src/Models/User.cs
+++ Backend/Src/Models/User.cs
@@ -4,5 +4,13 @@ namespace Company.ClassLibrary1;
 
 public class User
 {
-
+    public int Id { get; set; }
+    public required string Names { get; set; };
+    public required string LastNames { get; set; };
+    public required string Email { get; set; };
+    public required string PhoneNumber { get; set; };
+    public required DateOnly BirthDate { get; set; };
+    public required string Password { get; set; };
+    // Class Adress not needed on register form
+    public Adress? Adress { get; set; };
 }
