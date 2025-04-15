6770b2927556b6439415697ec60600f2ec9a2fc5
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 11:49:50 -0400

    feat(DTOs): Added UserDTO parameters

--- Backend/Src/DTOs/UserDTO.cs
+++ Backend/Src/DTOs/UserDTO.cs
@@ -2,5 +2,10 @@
 
 public class UserDTO
 {
-
+    public string Names { get; set; }
+    public string LastNames { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string Password { get; set; }
 }
