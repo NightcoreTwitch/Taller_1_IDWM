77e6528465268df6ecc84a0b14157c6eff39bf19
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 19:54:36 -0400

    Squashed commit of the following:
    commit 875b679e52e3f8ec221b6f260d339a71c969150a
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 19:32:22 2025 -0400
    
        feat(LoginDTO): added attributes and validations to login
    
    commit 46d4f07a7bf55d7a09bf49f9d2ced5cbec43a1ba
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 19:20:39 2025 -0400
    
        feat: added LoginDTO
    
    commit c939e5815b2f54398e50f4abb15954a19fb854ad
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 13:11:12 2025 -0400
    
        feat(RegisterDTO): added RegisterDTO attributes and validations
    
    commit 2e76b96cb0f085f0f05c0ceaabac238be4df1b57
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 13:02:58 2025 -0400
    
        feat: Added RegisterDTO
    
    commit 1d2270d0f635a2061f2742a545218ab1b42b0d6c
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 13:01:23 2025 -0400
    
        feat(Adress): added Adress attributes and Foreign key to User
    
    commit 3d1f2ebc9beb95d5e38af574047efe069b45605f
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 12:39:35 2025 -0400
    
        feat: Added Adress model
    
    commit 7bbdb129d11cd889bc84d099cd889ebbe63621a7
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 12:36:29 2025 -0400
    
        feat(Userdto): added UserDTO regular expressions
    
    commit 7a881445cbf53ba01b9b965f2035a61ef8af3ca3
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 11:58:57 2025 -0400
    
        feat(User): added User parameters that Admins can view
    
    commit 6770b2927556b6439415697ec60600f2ec9a2fc5
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 11:49:50 2025 -0400
    
        feat(DTOs): Added UserDTO parameters
    
    commit f1b83d41d55006dc65261cd3450930d23301859f
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 11:07:54 2025 -0400
    
        feat(DTOs): added UserDTO
    
    commit b35f584a495a9fe712be5b204e19d90163a08daa
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 11:01:00 2025 -0400
    
        feat(User): added User parameters
    
    commit 743bcba25a9a859447e12fec1678f97a7b64e605
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Tue Apr 15 10:40:59 2025 -0400
    
        feat: added User model

---
+++ Backend/Src/DTOs/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend;
+
+public class LoginDTO
+{
+    [Required(ErrorMessage = "Email is required.")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public required string Password { get; set; }
+}
