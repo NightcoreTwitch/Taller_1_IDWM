875b679e52e3f8ec221b6f260d339a71c969150a
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 19:32:22 -0400

    feat(LoginDTO): added attributes and validations to login

--- Backend/Src/DTOs/LoginDTO.cs
+++ Backend/Src/DTOs/LoginDTO.cs
@@ -2,5 +2,9 @@
 
 public class LoginDTO
 {
+    [Required(ErrorMessage = "Email is required.")]
+    public required string Email { get; set; }
 
+    [Required(ErrorMessage = "Password is required.")]
+    public required string Password { get; set; }
 }
