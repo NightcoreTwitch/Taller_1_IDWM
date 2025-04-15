1d2270d0f635a2061f2742a545218ab1b42b0d6c
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 13:01:23 -0400

    feat(Adress): added Adress attributes and Foreign key to User

--- Backend/Src/Models/Adress.cs
+++ Backend/Src/Models/Adress.cs
@@ -2,5 +2,13 @@
 
 public class Adress
 {
-
+    public int Id { get; set; }
+    public required string Street { get; set; }
+    public required string Number { get; set; }
+    public required string City { get; set; }
+    public required string Region { get; set; }
+    public required string ZipCode { get; set; }
+    // Foreign key to User
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
 }
