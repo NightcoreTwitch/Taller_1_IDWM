3bc63aef830127ac87ddb306fa1c030abe00b2ef
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 20:39:49 -0400

    fix: deleted User semicolons errors

--- Backend/Src/Models/User.cs
+++ Backend/Src/Models/User.cs
@@ -5,17 +5,17 @@ namespace Company.ClassLibrary1;
 public class User
 {
     public int Id { get; set; }
-    public required string Names { get; set; };
-    public required string LastNames { get; set; };
-    public required string Email { get; set; };
-    public required string PhoneNumber { get; set; };
-    public required DateOnly BirthDate { get; set; };
-    public required string Password { get; set; };
+    public required string Names { get; set; }
+    public required string LastNames { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required DateOnly BirthDate { get; set; }
+    public required string Password { get; set; }
     // Only Admins can view this field
     public string Status { get; set; } = "Active";
     public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     public DateOnly LastLogin { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
     // Class Adress not needed on register form
-    public Adress? Adress { get; set; };
+    public Adress? Adress { get; set; }
 }
