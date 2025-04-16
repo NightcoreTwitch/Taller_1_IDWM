e527f93d0d330ca1a03fabd78f986997927bc4e0
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-16 15:55:05 -0400

    feat(IUserRepository): added Login and Register functions interface

--- Backend/Src/Interfaces/IUserRepository.cs
+++ Backend/Src/Interfaces/IUserRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace Backend;
+﻿using System.Threading.Tasks;
+namespace Backend;
 
 public interface IUserRepository
 {
-
+    Task<UserDTO> AddUserAsync(RegisterDTO user);
+    Task<UserDTO> LoginAsync(LoginDTO user);
+    Task<UserDTO> GetUserByEmailAsync(string email);
 }
