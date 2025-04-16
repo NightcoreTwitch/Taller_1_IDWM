92e98b6a073cc554695696cde3beb4e7f0648356
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-16 15:16:42 -0400

    feat(DataSeeder): added Adress table seeder

--- Backend/Src/Data/DataSeeder.cs
+++ Backend/Src/Data/DataSeeder.cs
@@ -19,30 +19,42 @@ public class DataSeeder
     {
         context.Database.Migrate();
 
-        if (context.Users.Any())
-            return;
-        
-        var admin1 = new User(){
+        if (!context.Users.Any()){
+            var admin1 = new User(){
             Names = "Ignacio",
             LastNames = "Mancilla",
             Email = "ignacio.mancilla@gmail.com",
             PhoneNumber = "957519245",
             BirthDate = new DateOnly(2002, 08, 31),
             Password = "Pa$$word2025"
-        };
+            };
 
-        var faker = new Faker<User>()
-            .RuleFor(u => u.Names, f => f.Name.FirstName())
-            .RuleFor(u => u.LastNames, f => f.Name.LastName())
-            .RuleFor(u => u.Email, f => f.Internet.Email())
-            .RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber())
-            .RuleFor(u => u.BirthDate, f => DateOnly.FromDateTime(f.Date.Past(18, DateTime.Now)))
-            .RuleFor(u => u.Password, f => f.Internet.Password());
+            var faker = new Faker<User>()
+                .RuleFor(u => u.Names, f => f.Name.FirstName())
+                .RuleFor(u => u.LastNames, f => f.Name.LastName())
+                .RuleFor(u => u.Email, f => f.Internet.Email())
+                .RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber())
+                .RuleFor(u => u.BirthDate, f => DateOnly.FromDateTime(f.Date.Past(18, DateTime.Now)))
+                .RuleFor(u => u.Password, f => f.Internet.Password());
 
-        var users = faker.Generate(9);
+            var users = faker.Generate(9);
 
-        context.Set<User>().Add(admin1);
-        context.Set<User>().AddRange(users);
-        context.SaveChanges();
+            context.Set<User>().Add(admin1);
+            context.Set<User>().AddRange(users);
+            context.SaveChanges();
+        }
+        if (!context.Adresses.Any()){
+            var adminAdress1 = new Adress(){
+                Street = "Hernan Cortes",
+                Number = 2637,
+                City = "Calama",
+                Region = "Antofagasta",
+                ZipCode = "1390000",
+                UserId = 1
+            };
+
+            context.Set<Adress>().AddRange(adminAdress1);
+            context.SaveChanges();
+        }
     }
 }
