2f9e4b8be5a48d2a7d1550ebc8896914337d4eb0
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-15 20:36:48 -0400

    feat(Program): added initializer DataContext and DataSeeder

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,14 +5,14 @@ DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options => 
 {
@@ -25,6 +25,8 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+DataSeeder.InitDb(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
