f621d5028d1d54bdcf81fa9e88b499dd5fe7e0a3
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-16 17:48:46 -0400

    feat(UserRepository): added AddUserAsync function

--- Backend/Src/Repositories/UserRepository.cs
+++ Backend/Src/Repositories/UserRepository.cs
@@ -8,6 +8,13 @@ public class UserRepository : IUserRepository
     {
         _context = context;
     }
+    public async Task<UserDTO> AddUserAsync(RegisterDTO user)
+    {
+        var newUser = UserMapper.MapToUser(user);
+        await _context.Users.AddAsync(newUser);
+        await _context.SaveChangesAsync();
+        return UserMapper.MapToUserDTO(newUser);
+    }
     public async Task<UserDTO> GetUserByEmailAsync(string email)
     {
         var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
