5886b687b55d5a2ba04d6775aca5e6b822caa3de
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-16 16:04:26 -0400

    feat(UserMapper): added registerdto to userdto and userdto to user mappers

--- Backend/Src/Mappers/UserMapper.cs
+++ Backend/Src/Mappers/UserMapper.cs
@@ -1,6 +1,29 @@
-﻿namespace Backend;
+﻿namespace Backend.Src.Mappers;
 
 public class UserMapper
 {
-
+    public static UserDTO => MapToUserDTO(RegisterDTO registerDTO)
+    {
+        return new UserDTO
+        {
+            Names = registerDTO.Names,
+            LastNames = registerDTO.LastNames,
+            Email = registerDTO.Email,
+            PhoneNumber = registerDTO.PhoneNumber,
+            BirthDate = registerDTO.BirthDate,
+            Password = registerDTO.Password
+        };
+    }
+    public static User MapToUser(UserDTO userDTO)
+    {
+        return new User
+        {
+            Names = userDTO.Names,
+            LastNames = userDTO.LastNames,
+            Email = userDTO.Email,
+            PhoneNumber = userDTO.PhoneNumber,
+            BirthDate = userDTO.BirthDate,
+            Password = userDTO.Password
+        };
+    }
 }
