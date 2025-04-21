9e478038058132e73241311dc2f06311700ed73b
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-20 21:22:35 -0400

    Squashed commit of the following:
    commit 9f0fc897f8c2ba4ea19240545a1ad000392af9c2
    Author: Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
    Date:   Sun Apr 20 21:21:26 2025 -0400
    
        feat(UserMapper): added MapToUser function

--- Backend/Src/Mappers/UserMapper.cs
+++ Backend/Src/Mappers/UserMapper.cs
@@ -1,4 +1,9 @@
-﻿namespace Backend.Src.Mappers;
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Backend.Src.DTOs;
+using Backend.Src.Models;
+
+namespace Backend.Src.Mappers;
 
 public class UserMapper
 {
@@ -24,6 +29,18 @@ public class UserMapper
             PhoneNumber = userDTO.PhoneNumber,
             BirthDate = userDTO.BirthDate,
             Password = userDTO.Password
+        };
+    }
+    public static UserDTO MapToUserDTO(User user)
+    {
+        return new UserDTO
+        {
+            Names = user.Names,
+            LastNames = user.LastNames,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            BirthDate = user.BirthDate,
+            Password = user.Password
         };
     }
 }
