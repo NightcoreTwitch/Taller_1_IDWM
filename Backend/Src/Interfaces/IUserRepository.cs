a95f56f06b22380bb67ecd94cdbb5eaa0317a89d
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-22 18:19:40 -0400

    fix(IUserRepository): fixed return data types

--- Backend/Src/Interfaces/IUserRepository.cs
+++ Backend/Src/Interfaces/IUserRepository.cs
@@ -1,13 +1,10 @@
-﻿using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Backend.Src.Data;
-using Backend.Src.DTOs;
+﻿using Backend.Src.DTOs;
 
 namespace Backend.Src.Interfaces;
 
 public interface IUserRepository
 {
-    Task<UserDTO> AddUserAsync(RegisterDTO user);
-    Task<UserDTO> LoginAsync(LoginDTO user);
+    Task<string> AddUserAsync(RegisterDTO user);
+    Task<string> LoginAsync(LoginDTO user);
     Task<UserDTO> GetUserByEmailAsync(string email);
 }
