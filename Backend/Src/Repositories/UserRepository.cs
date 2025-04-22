a0f9a28966ee5a932b66217643f6b68d47772dbc
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-22 18:20:46 -0400

    feat(UserRepository): added JWT tokens to validations

--- Backend/Src/Repositories/UserRepository.cs
+++ Backend/Src/Repositories/UserRepository.cs
@@ -1,38 +1,43 @@
-﻿using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Backend.Src.Data;
 using Backend.Src.DTOs;
 using Backend.Src.Interfaces;
 using Backend.Src.Mappers;
-using Backend.Src.Models;
 
 namespace Backend.Src.Repositories;
 
 public class UserRepository : IUserRepository
 {
     private readonly DataContext _context;
-
-    public UserRepository(DataContext context)
+    private readonly ITokenRepository _tokenRepository;
+    public UserRepository(DataContext context, ITokenRepository tokenRepository)
     {
         _context = context;
+        _tokenRepository = tokenRepository;
     }
-    public async Task<UserDTO> AddUserAsync(RegisterDTO user)
+    public async Task<string> AddUserAsync(RegisterDTO user)
     {
+        var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+        if (existingUser != null)
+        {
+            return null;
+        }
+
         var newUser = UserMapper.MapToUser(user);
         await _context.Users.AddAsync(newUser);
         await _context.SaveChangesAsync();
-        return UserMapper.MapToUserDTO(newUser);    
+
+        return await _tokenRepository.GenerateTokenAsync(newUser);
     }
-    public async Task<UserDTO> LoginAsync(LoginDTO user)
+    public async Task<string> LoginAsync(LoginDTO user)
     {
-        var existingUser = await _context.Users.FirstOrDefaultAsync(u => 
-            u.Email == user.Email &&
-            u.Password == user.Password);
+        var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email && u.Password == user.Password);
         if (existingUser == null)
         {
             return null;
         }
-        return UserMapper.MapToUserDTO(existingUser);
+
+        return await _tokenRepository.GenerateTokenAsync(existingUser);
     }
     public async Task<UserDTO> GetUserByEmailAsync(string email)
     {
