ca583889ce33394b63c58aee0971c5384f333cb3
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-22 10:22:47 -0400

    feat(TokenRepository): added CreateTokenAsync function

--- Backend/Src/Repositories/TokenRepository.cs
+++ Backend/Src/Repositories/TokenRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace Backend;
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Backend.Src.DTOs;
+using Backend.Src.Interfaces;
+using Backend.Src.Models;
 
-public class TokenRepository
+namespace Backend.Src.Repositories;
+
+public class TokenRepository : ITokenRepository
 {
+    private readonly IConfiguration _configuration;
 
+    public TokenRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<string> CreateTokenAsync(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.UserName)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
 }
