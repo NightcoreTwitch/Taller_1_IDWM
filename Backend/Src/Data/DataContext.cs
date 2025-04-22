47a0336a95ddca785ea08379e7b40de2806af64d
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-22 18:40:19 -0400

    feat(DataContext): added identity tables and Roles

--- Backend/Src/Data/DataContext.cs
+++ Backend/Src/Data/DataContext.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Backend.Src.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Backend.Src.Data;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<User, IdentityRole<int>, int>
 {
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
-    {
-    }
-
-    public DbSet<User> Users { get; set; }
     public DbSet<Adress> Adresses { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole<int>>().HasData(
+            new IdentityRole<int> { Id = 1, Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole<int> { Id = 2, Name = "User", NormalizedName = "USER" }
+        );
+    }
 }
