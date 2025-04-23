f6f3b4fe0c8fb098ec3d7503b9968ea5f9069103
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-22 20:29:23 -0400

    feat(Program): added identity and JWT to DBContext and validations

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,6 +5,9 @@ using Backend.Src.Data;
 using Backend.Src.Models;
 using Backend.Src.Interfaces;
 using Backend.Src.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
 
 DotEnv.Load();
 
@@ -13,8 +16,45 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.User.RequireUniqueEmail = true;
+})
+.AddRoles<IdentityRole<int>>()
+.AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        LifetimeValidator = (notBefore, expires, token, parameters) => expires > DateTime.UtcNow,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<RoleManager<IdentityRole<int>>>();
+builder.Services.AddScoped<UserManager<User>>();
 
 builder.Services.AddCors(options => 
 {
@@ -25,8 +65,6 @@ builder.Services.AddCors(options =>
             builder.AllowAnyMethod();
         });
 });
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
@@ -40,8 +78,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseSwagger();
 app.UseSwaggerUI();
-//app.UseAuthentication();
-//app.UseAuthorization();
-//app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
