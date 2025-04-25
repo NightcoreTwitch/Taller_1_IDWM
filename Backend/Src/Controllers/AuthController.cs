e0c2561633da402f330372b4164739782fc759ae
Nightcore_Twitch <gabriel.cruz@alumnos.ucn.cl>
2025-04-25 17:30:09 -0400

    feat(AuthController): added standar to error json

--- Backend/Src/Controllers/AuthController.cs
+++ Backend/Src/Controllers/AuthController.cs
@@ -33,22 +33,74 @@ public class AuthController : ControllerBase
     {
         if (registerDTO == null)
         {
-            return BadRequest("Invalid data.");
+            var errors = new Dictionary<string, string[]>
+            {
+                { "Null", new[] { "The model is null" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
         }
         if (!ModelState.IsValid)
         {
-            return BadRequest(ModelState);
+            var errors = new Dictionary<string, string[]>
+            {
+                { "ModelState", new[] { "Invalid model" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
         }
         if (registerDTO.Password != registerDTO.ConfirmPassword)
         {
-            return BadRequest("Passwords do not match.");
+            var errors = new Dictionary<string, string[]>
+            {
+                { "Password", new[] { "Passwords do not match" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
+        }
+        if (await _userManager.FindByEmailAsync(registerDTO.Email) != null)
+        {
+            var errors = new Dictionary<string, string[]>
+            {
+                { "Email", new[] { "Email already exists" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
         }
         var newUser = UserMapper.MapToUser(registerDTO);
         newUser.UserName = newUser.Email;
         var result = await _userManager.CreateAsync(newUser, registerDTO.Password);
         if (!result.Succeeded)
         {
-            return BadRequest("User creation failed.");
+            return BadRequest(result.Errors.Select(e => e.Description).ToList());
         }
         if (result.Succeeded)
         {
@@ -58,7 +110,7 @@ public class AuthController : ControllerBase
                 return BadRequest("User role assignment failed.");
             }
             var roleNewUser = await _userManager.GetRolesAsync(newUser);
-            var token = await _tokenRepository.GenerateTokenAsync(newUser, roleNewUser);
+            var token = await _tokenRepository.GenerateTokenAsync(newUser, roleNewUser.FirstOrDefault());
             return Ok(new { FullName = newUser.Names + " " + newUser.LastNames, Email = newUser.Email, Token = token });
         }
         return BadRequest("User creation failed.");
@@ -69,24 +121,70 @@ public class AuthController : ControllerBase
     {
         if (loginDTO == null)
         {
-            return BadRequest("Invalid data.");
+            var errors = new Dictionary<string, string[]>
+            {
+                { "Null", new[] { "The model is null" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
         }
         if (!ModelState.IsValid)
         {
-            return BadRequest(ModelState);
+            var errors = new Dictionary<string, string[]>
+            {
+                { "ModelState", new[] { "Invalid model" } }
+            };
+
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+
+            return BadRequest(problemDetails);
         }
         var user = await _userManager.FindByEmailAsync(loginDTO.Email);
         if (user == null)
         {
-            return Unauthorized("User or password is incorrect.");
+            var errors = new Dictionary<string, string[]>
+            {
+                { "User or Password", new[] { "User or password is incorrect." } }
+            };
+            
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status401Unauthorized,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+            return BadRequest(problemDetails);
         }
         var result = await _userManager.CheckPasswordAsync(user, loginDTO.Password);
         if (!result)
         {
-            return Unauthorized("User or password is incorrect.");
+            var errors = new Dictionary<string, string[]>
+            {
+                { "User or Password", new[] { "User or password is incorrect." } }
+            };
+            
+            var problemDetails = new ValidationProblemDetails(errors)
+            {
+                Title = "One or more validation errors occurred.",
+                Status = StatusCodes.Status401Unauthorized,
+                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1"
+            };
+            return BadRequest(problemDetails);
         }
         var role = await _userManager.GetRolesAsync(user);
-        var token = await _tokenRepository.GenerateTokenAsync(user, role);
+        var token = await _tokenRepository.GenerateTokenAsync(user, role.FirstOrDefault());
         return Ok(new { Token = token });
     }
 }
